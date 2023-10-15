013d08719054242af7e575bc20dbaee987076ab6
wenxuanjun <wenxuanjun@users.noreply.github.com>
2023-10-15 06:47:20 +0000

    Github CI plos-clan/PlosClan-Source@21b8374c2dbcd50bee3e0e2cd4f1bd68d03996ad

---
+++ assets/404-f87e19f8.js
@@ -0,0 +1,1 @@
+import{t}from"./index-ddcd419b.js";const s=t(`<section class="p-8 text-gray-700"><h1 class="text-2xl font-bold">404: Not Found</h1><p class=mt-4>It's gone 😞`),e=()=>s();export{e as default};
