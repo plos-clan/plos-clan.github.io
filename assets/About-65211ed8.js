fe58213eba63429e14b568b0bed5e8727aabd9d5
wenxuanjun <wenxuanjun@users.noreply.github.com>
2023-10-15 05:49:37 +0000

    Github CI plos-clan/PlosClan-Source@b0afc7de67f0cb26ce0df1fa4347819744304255

---
+++ assets/About-65211ed8.js
@@ -0,0 +1,1 @@
+import{t}from"./index-b0de39dd.js";const s=t('<section class="bg-pink-100 p-8 text-gray-700"><h1 class="text-2xl font-bold">关于</h1><p class=mt-4>这是一个奇怪的关于页面。'),a=()=>s();export{a as default};
