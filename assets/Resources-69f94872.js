7efb6e1f00e1737e3ae832ff031a00f57180edac
wenxuanjun <wenxuanjun@users.noreply.github.com>
2023-10-17 04:00:44 +0000

    Github CI plos-clan/PlosClan-Source@11a65f869b5f7b6924efb427ea7691d5d6f71c76

---
+++ assets/Resources-69f94872.js
@@ -0,0 +1,1 @@
+import{t}from"./index-c00d3a6e.js";const s=t('<section class="bg-blue-50 p-8 text-gray-700"><h1 class="text-2xl font-bold">资源下载</h1><p class=mt-4>现在这里真的什么也没有啊（悲）'),l=()=>s();export{l as default};
--- assets/Resources-cd86e648.js
+++
@@ -1,1 +0,0 @@
-import{t}from"./index-0f4284ff.js";const s=t('<section class="bg-blue-50 p-8 text-gray-700"><h1 class="text-2xl font-bold">资源下载</h1><p class=mt-4>现在这里真的什么也没有啊（悲）'),l=()=>s();export{l as default};
