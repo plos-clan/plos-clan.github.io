bb89870d6118b453b8cdcc759e020f70a8aa5381
CLimber-Rong <CLimber-Rong@users.noreply.github.com>
2023-10-23 09:41:00 +0000

    Github CI plos-clan/PlosClan-Source@b79f81fb123707019c05cf00915154632f5de3fe

---
+++ assets/404-4db09848.js
@@ -0,0 +1,1 @@
+import{t}from"./index-9b4b8bd4.js";const s=t(`<section class="p-8 text-gray-700"><h1 class="text-2xl font-bold">404: Not Found</h1><p class=mt-4>It's gone 😞`),e=()=>s();export{e as default};
---
+++ assets/Posts-c559f9f9.js
@@ -0,0 +1,44 @@
+import{b as s,t as n}from"./index-9b4b8bd4.js";const a=n("<meta charset=UTF-8>"),d=n("<title>若智新闻"),p=n("<style>"),c=n("<h1><s>若智新闻</s>整活贴吧"),_=n("<div>(OS群奇闻异事)若智升天，我即是仙"),y=n("<ul><li><a href=https://plos-clan.github.io>OS群网站首页</a></li><li><a>最新消息</a></li><li><a href=javascript:void(0); onclick=ws_about()>关于"),h=n("<div><br>"),$=n("<div id=wsm>"),m=n("<div id=byd>"),b=n("<div id=tish>"),x=()=>[a(),d(),(()=>{const t=p();return s(t,"dangerouslysetinnerhtml",{__html:`
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+            box-shadow: 2px 2px 5px #333;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover {
+            background-color: #111;
+        }
+
+        a:link {
+            text-decoration: none;
+            color: black;
+        }
+
+        a:visited {
+            text-decoration: none;
+            color: #333
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        a:active {
+            text-decoration: underline;
+        }
+    `}),t})(),(()=>{const t=c();return t.style.setProperty("display","inline"),t.style.setProperty("color","brown"),t})(),(()=>{const t=_();return t.style.setProperty("display","inline"),t.style.setProperty("color","rgb(126, 126, 126)"),t})(),(()=>{const t=y(),e=t.firstChild,l=e.firstChild,o=e.nextSibling,i=o.nextSibling,r=i.firstChild;return l.style.setProperty("color","white"),r.style.setProperty("color","white"),t})(),h(),$(),m(),b()];export{x as default};
